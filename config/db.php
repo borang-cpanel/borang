45e8bab89baafef33e27fae5287091f434850ee1
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2022-12-22 00:07:40 +0700

    update config

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=borang_pori',
     'username' => 'root',
-    'password' => '',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
